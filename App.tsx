cb6dc200bd615e7733c9031f43b8b7b38618adbf
Mahmut Can Kahya <mahmutcankahya@windowslive.com>
2023-03-23 12:35:45 +0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,48 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import NfcManager, {NfcTech} from 'react-native-nfc-manager';
+
+// Pre-step, call this before any NFC operations
+NfcManager.start();
+
+function App(): JSX.Element {
+  async function readNdef() {
+    try {
+      // register for the NFC tag with NDEF in it
+      await NfcManager.requestTechnology(NfcTech.Ndef);
+      // the resolved tag object will contain `ndefMessage` property
+      const tag = await NfcManager.getTag();
+      console.warn('Tag found', tag);
+    } catch (ex) {
+      console.warn('Oops!', ex);
+    } finally {
+      // stop the nfc scanning
+      NfcManager.cancelTechnologyRequest();
+    }
+  }
+
+  return (
+    <View style={styles.wrapper}>
+      <TouchableOpacity onPress={readNdef}>
+        <Text>Scan a Tag</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default App;
